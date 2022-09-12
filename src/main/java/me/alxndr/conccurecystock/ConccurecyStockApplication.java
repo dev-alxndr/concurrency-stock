489c3ea073041466a86a7fd97a105762d5faa7e6
Alexander Choi <dev.alxndr@gmail.com>
2022-09-12 14:51:10 +0900

    init project

---
+++ src/main/java/me/alxndr/conccurecystock/ConccurecyStockApplication.java
@@ -0,0 +1,13 @@
+package me.alxndr.conccurecystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConccurecyStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConccurecyStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/alxndr/conccurecystock/ConccurecyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alxndr.conccurecystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConccurecyStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
