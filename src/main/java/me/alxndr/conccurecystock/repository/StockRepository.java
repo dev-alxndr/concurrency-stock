87d819820b22e17768861139d9fa605b7ed5468a
Alexander Choi <dev.alxndr@gmail.com>
2022-09-12 15:37:44 +0900

    PessimisticLock 을 활용한 제어

--- src/main/java/me/alxndr/conccurecystock/repository/StockRepository.java
+++ src/main/java/me/alxndr/conccurecystock/repository/StockRepository.java
@@ -2,10 +2,18 @@ package me.alxndr.conccurecystock.repository;
 
 import me.alxndr.conccurecystock.domain.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.LockModeType;
 
 /**
  * @author : Alexander Choi
  * @date : 2022/09/12
  */
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :id")
+    Stock findByIdWithPessimisticLock(Long id);
 }
--- src/test/java/me/alxndr/conccurecystock/service/StockServiceTest.java
+++ src/test/java/me/alxndr/conccurecystock/service/StockServiceTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
