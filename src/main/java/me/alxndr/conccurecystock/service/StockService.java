2c7b0629fcbb2682437bf8f5fc360832cd6fce7c
Alexander Choi <dev.alxndr@gmail.com>
2022-09-12 15:24:29 +0900

    @Transactional 삭제
    - 정상적으로 Syncronized 작동

--- src/main/java/me/alxndr/conccurecystock/service/StockService.java
+++ src/main/java/me/alxndr/conccurecystock/service/StockService.java
@@ -16,12 +16,14 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    @Transactional
+
     public synchronized void decrease(Long id, Long quantity) {
 
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
+        stockRepository.saveAndFlush(stock);
     }
+}
 
 
 
